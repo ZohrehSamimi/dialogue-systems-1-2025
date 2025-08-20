36845eae739fcc9f329ce824cbe5db5201bd6917
ZohrehSamimi <144061318+ZohrehSamimi@users.noreply.github.com>
2025-08-20 11:49:29 +0200

    Update azure.ts

--- Code/src/azure.ts
+++ Code/src/azure.ts
@@ -1,1 +1,7 @@
-export const KEY = "1wkAREoXFI31nPfAcFzfxIPKE1jjqaNBJdrX57eYOkSStSbaUUEAJQQJ99BEACi5YpzXJ3w3AAAaACOGwvNz"; // if you use it elsewhere
+export const KEY = "DShyFSZAlA9EMig6eEktPMkkjA7DPWlbCHFnLA9onrLVamJQ8ufuJQQJ99BBACi5YpzXJ3w3AAAYACOG3fBB"; 
+export const SPEECH_ENDPOINT = "https://northeurope.api.cognitive.microsoft.com/";
+// CLU (NLU) key
+export const NLU_KEY = "46hrP9d7QhNTqodDB6Jmehbg2sDMiPoZOoy9P1XRMFMmZgObdKG1JQQJ99BEACi5YpzXJ3w3AAAaACOGET4o";
+export const NLU_ENDPOINT = "https://lab4samimi.cognitiveservices.azure.com/";
+export const NLU_PROJECT = "appointment";
+export const NLU_DEPLOYMENT = "appointment";
